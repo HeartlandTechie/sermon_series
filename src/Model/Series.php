15bbe9b5af8761fcb042c133a2964615e44ae015
Bradley Miller <bradley.w.miller@gmail.com>
2023-03-28 22:46:57 -0500

    Updated structure

--- src/Model/Series.php
+++ src/Model/Series.php
@@ -4,6 +4,17 @@ namespace HearlandTechie\SermonSeries\Model;
 
 use SilverStripe\ORM\DataObject;
 
+use SilverStripe\Forms\FieldList;
+use SilverStripe\Forms\TextField;
+use SilverStripe\Forms\DropdownField;
+use SilverStripe\Forms\CurrencyField;
+use SilverStripe\Forms\CheckboxField;
+use SilverStripe\AssetAdmin\Forms\UploadField;
+use SilverStripe\ORM\ArrayLib;
+use SilverStripe\Assets\Image;
+use SilverStripe\Forms\TabSet;
+
+
 class Series extends DataObject
 {
     private static $db = [
@@ -11,4 +22,17 @@ class Series extends DataObject
         'SeriesName' => 'Varchar(255)',
         'DateBegins' => 'Date'
     ];
+
+
+    public function getCMSfields()
+    {
+        $fields = FieldList::create(TabSet::create('Root'));
+        $fields->addFieldsToTab('Root.Main', [
+            TextField::create('SeriesName'),
+
+
+        ]);
+
+        return $fields;
+    }
 }
