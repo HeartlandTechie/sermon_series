7482e961094b517440a3aff4693e91688f988898
Bradley Miller <bradley.w.miller@gmail.com>
2023-03-29 05:34:48 -0500

    Updated structure

---
+++ src/Controller/SeriesAdmin.php
@@ -0,0 +1,17 @@
+<?php
+namespace HearlandTechie\SermonSeries\Controller;
+
+use SilverStripe\Admin\ModelAdmin;
+use HearlandTechie\SermonSeries\Model\Series;
+
+class SeriesAdmin extends ModelAdmin
+{
+
+private static $menu_title = 'Series';
+
+private static $url_segment = 'series';
+
+private static $managed_models = [
+Series::class,
+];
+}
