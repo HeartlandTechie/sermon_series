8c6bbd8397a7014b29e6f209d6e38b0b4e2ddb41
Bradley Miller <bradley.w.miller@gmail.com>
2023-04-20 20:30:27 -0500

    Updated structure

--- src/Controller/SeriesPageController.php
+++ src/Controller/SeriesPageController.php
@@ -6,4 +6,13 @@ use PageController;
 class SeriesPageController extends PageController
 {
 
+    private static $allowed_actions = [
+        'test'
+    ];
+
+    public function test()
+    {
+        die('it works');
+    }
+
 }
