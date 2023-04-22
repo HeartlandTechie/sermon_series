26a1f48efb40bb080de3276d82f12ef6ac60346d
Bradley Miller <bradley.w.miller@gmail.com>
2023-04-22 09:37:55 -0500

    Updated structure

---
+++ src/Controller/HomePage.php
@@ -0,0 +1,9 @@
+<?php
+namespace HearlandTechie\SermonSeries\Controller;
+
+use Page;
+
+class HomePage extends Page
+{
+
+}
---
+++ src/Controller/HomePageController.php
@@ -0,0 +1,10 @@
+<?php
+namespace HearlandTechie\SermonSeries\Controller;
+
+
+use PageController;
+
+class HomePageController extends PageController
+{
+
+}
