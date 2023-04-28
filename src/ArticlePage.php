1a52aa9cae607dccf343723cb5f47b4f6873841a
Bradley Miller <bradley.w.miller@gmail.com>
2023-04-27 20:04:19 -0500

    Updated structure

--- src/ArticlePage.php
+++ src/ArticlePage.php
@@ -9,4 +9,18 @@ class ArticlePage extends Page
 
     private static $can_be_root = false;
 
+
+    private static $db = [
+        'Date' => 'Date',
+        'Teaser' => 'Text',
+        'Author' => 'Varchar',
+    ];
+
+    public function getCMSFields()
+    {
+        $fields = parent::getCMSFields();
+
+        return $fields;
+    }
+
 }
