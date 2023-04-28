ca3669c4c3d3bfbd1df0ba07ee11c7cfbd6aad8a
Bradley Miller <bradley.w.miller@gmail.com>
2023-04-27 20:06:55 -0500

    Updated structure

--- src/ArticlePage.php
+++ src/ArticlePage.php
@@ -4,6 +4,12 @@ namespace HeartlandTechie\SermonSeries;
 
 use Page;
 
+
+use SilverStripe\Forms\DateField;
+use SilverStripe\Forms\TextareaField;
+use SilverStripe\Forms\TextField;
+
+
 class ArticlePage extends Page
 {
 
@@ -16,9 +22,14 @@ class ArticlePage extends Page
         'Author' => 'Varchar',
     ];
 
-    public function getCMSFields()
+// ...
+
+        public function getCMSFields()
     {
         $fields = parent::getCMSFields();
+        $fields->addFieldToTab('Root.Main', DateField::create('Date','Date of article'));
+        $fields->addFieldToTab('Root.Main', TextareaField::create('Teaser'));
+        $fields->addFieldToTab('Root.Main', TextField::create('Author','Author of article'));
 
         return $fields;
     }
